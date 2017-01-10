3c06a77ff75b96f0a17f5da58f6cdbf0957f98d3
Samael Olascoaga <olaskuaga@gmail.com>
2017-01-09 21:59:19 -0600

    Rabbits and Recurrence Relations

---
+++ FIB.py
@@ -0,0 +1,23 @@
+"""
+
+Problem: Rabbits and Recurrence Relations
+ID: FIB
+URL: http://rosalind.info/problems/fib/
+Solution author: Samael Olascoaga
+
+"""
+
+def fibo(n, k):
+	if n == 1:
+		return 1
+	elif n == 2:
+		return k
+	else:
+		one_gen = fibo(n - 1, k)
+		second_gen = fibo(n - 2, k)
+	if n <= 4:
+		return(one_gen + second_gen)
+
+	return (one_gen + (second_gen * k))
+
+print(fibo(5, 3))
